54d8ab20cf3a800d5028e219ca10829cfdbfecbc
Mosibun <142289033+Mosibun@users.noreply.github.com>
2024-03-28 16:39:26 +0600

    Add files via upload

---
+++ answer.js
@@ -0,0 +1,422 @@
+const quizData = {
+
+    '2024-03-28': [
+        {
+            question: "Which continent is home to the Amazon Rainforest, the largest tropical rainforest in the world?",
+            options: ["South America", "Africa", "Asia", "Australia"],
+            correctAnswer: "South America"
+        },
+        {
+            question: "What percentage of the Earth's land area is covered by forests?",
+            options: ["Approximately 30%", "Approximately 50%", "Approximately 20%", "Approximately 10%"],
+            correctAnswer: "Approximately 30%"
+        },
+        {
+            question: "What is the term for a forest that experiences extremely low temperatures and is dominated by coniferous trees?",
+            options: ["Deciduous Forest", "Tropical Rainforest", "Taiga", "Savanna"],
+            correctAnswer: "Taiga"
+        },
+        {
+            question: "Which country is known as the 'Lungs of Europe' due to its extensive forest cover?",
+            options: ["Sweden", "Germany", "France", "Russia"],
+            correctAnswer: "Sweden"
+        },
+        {
+            question: "What is the largest remaining tropical rainforest in Africa?",
+            options: ["Congo Rainforest", "Amazon Rainforest", "Borneo Rainforest", "Cerrado"],
+            correctAnswer: "Congo Rainforest"
+        }
+    ],    
+
+    '2024-03-29': [
+        {
+            question: "What is the process by which water vapor in the atmosphere condenses into droplets and falls to the Earth's surface?",
+            options: ["Condensation", "Precipitation", "Evaporation", "Transpiration"],
+            correctAnswer: "Precipitation"
+        },
+        {
+            question: "What is the term for rain that freezes on contact with surfaces, forming a layer of ice?",
+            options: ["Freezing Rain", "Sleet", "Hail", "Drizzle"],
+            correctAnswer: "Freezing Rain"
+        },
+        {
+            question: "What is the scientific name for the study of rain and other forms of precipitation?",
+            options: ["Meteorology", "Hydrology", "Pluviology", "Climatology"],
+            correctAnswer: "Pluviology"
+        },
+        {
+            question: "What is the world record for the most rainfall in a single year, as of 2024?",
+            options: ["11,871 millimeters", "8,646 millimeters", "6,857 millimeters", "13,455 millimeters"],
+            correctAnswer: "11,871 millimeters"
+        },
+        {
+            question: "What is the term for a prolonged period of abnormally low rainfall, leading to a shortage of water?",
+            options: ["Flood", "Monsoon", "Drought", "Typhoon"],
+            correctAnswer: "Drought"
+        }
+    ],    
+
+    '2024-03-30': [
+        {
+            question: "Which of the following is a synonym for 'Eloquent'?",
+            options: ["Silent", "Talkative", "Expressive", "Mysterious"],
+            correctAnswer: "Expressive"
+        },
+        {
+            question: "What is the plural form of the word 'Criterion'?",
+            options: ["Criterions", "Criteries", "Criteria", "Criteriums"],
+            correctAnswer: "Criteria"
+        },
+        {
+            question: "Which of these words is a conjunction?",
+            options: ["Quickly", "And", "Beautiful", "Jump"],
+            correctAnswer: "And"
+        },
+        {
+            question: "What is the past tense of the verb 'Swim'?",
+            options: ["Swam", "Swum", "Swimming", "Swime"],
+            correctAnswer: "Swam"
+        },
+        {
+            question: "Which of these is a figure of speech involving the comparison of one thing with another using 'like' or 'as'?",
+            options: ["Metaphor", "Simile", "Hyperbole", "Personification"],
+            correctAnswer: "Simile"
+        },
+    ],
+
+    '2024-03-31': [
+        {
+            question: "What is the script used to write the Bengali language?",
+            options: ["Devanagari", "Arabic", "Latin", "Bengali Script"],
+            correctAnswer: "Bengali Script"
+        },
+        {
+            question: "Who is considered the national poet of Bangladesh for his contributions to Bengali literature?",
+            options: ["Kazi Nazrul Islam", "Rabindranath Tagore", "Jibanananda Das", "Sukanta Bhattacharya"],
+            correctAnswer: "Kazi Nazrul Islam"
+        },
+        {
+            question: "What is the name of the traditional Bengali New Year celebration?",
+            options: ["Diwali", "Navratri", "Durga Puja", "Pohela Boishakh"],
+            correctAnswer: "Pohela Boishakh"
+        },
+        {
+            question: "What is the capital of Australia?",
+            options: ["Sydney", "Canberra", "Melbourne", "Perth"],
+            correctAnswer: "Canberra"
+        },
+        {
+            question: "What is the currency of Japan?",
+            options: ["Yuan", "Won", "Yen", "Ringgit"],
+            correctAnswer: "Yen"
+        },
+    ],
+
+    '2024-04-01': [
+        {
+            question: "Which ocean is the largest on Earth?",
+            options: ["Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Pacific Ocean"],
+            correctAnswer: "Pacific Ocean"
+        },
+        {
+            question: "Which ocean is the smallest and the shallowest among the world's oceans?",
+            options: ["Arctic Ocean", "Indian Ocean", "Southern Ocean", "Atlantic Ocean"],
+            correctAnswer: "Arctic Ocean"
+        },
+        {
+            question: "What is the term for a large area of the ocean that is partially enclosed by land?",
+            options: ["Sea", "Strait", "Gulf", "Bay"],
+            correctAnswer: "Sea"
+        },
+        {
+            question: "Which ocean is located between Africa, Europe, and the Americas?",
+            options: ["Arctic Ocean", "Atlantic Ocean", "Indian Ocean", "Pacific Ocean"],
+            correctAnswer: "Atlantic Ocean"
+        },
+        {
+            question: "What is the deepest known part of the world's oceans?",
+            options: ["Mariana Trench", "Puerto Rico Trench", "Sunda Trench", "Tonga Trench"],
+            correctAnswer: "Mariana Trench"
+        },
+    ],
+
+    '2024-04-02': [
+        {
+            question: "How many days are there in the month of February in 2016",
+            options: ["28", "29", "30", "31"],
+            correctAnswer: "29"
+        },
+        {
+            question: "Which zodiac signs are associated with the month of February?",
+            options: ["Leo and Virgo", "Aquarius and Pisces", "Aries and Taurus", "Gemini and Cancer"],
+            correctAnswer: "Aquarius and Pisces"
+        },
+        {
+            question: "In the Northern Hemisphere, which season does February fall into?",
+            options: ["Spring", "Summer", "Autumn", "Winter"],
+            correctAnswer: "Winter"
+        },
+        {
+            question: "Which famous American holiday is celebrated on the third Monday of February?",
+            options: ["Independence Day", "Memorial Day", "Labor Day", "Presidents' Day"],
+            correctAnswer: "Presidents' Day"
+        },
+        {
+            question: "What historical event is celebrated on February 14th?",
+            options: ["Veterans Day", "Thanksgiving", "Valentine's Day", "April Fools' Day"],
+            correctAnswer: "Valentine's Day"
+        },
+    ],
+
+    '2024-04-03': [
+        {
+            question: "Before becoming President, Joe Biden served as the Vice President under which U.S. President?",
+            options: ["Barack Obama", "George W. Bush", "Bill Clinton", "Donald Trump"],
+            correctAnswer: "Barack Obama"
+        },
+        {
+            question: "In which state was Joe Biden born?",
+            options: ["Delaware", "Pennsylvania", "New York", "Maryland"],
+            correctAnswer: "Pennsylvania"
+        },
+        {
+            question: "What is Joe Biden's middle name?",
+            options: ["John", "Robert", "Robinette", "Joseph"],
+            correctAnswer: "Robinette"
+        },
+        {
+            question: "Joe Biden was inaugurated as the 46th President of the United States in which year?",
+            options: ["2016", "2017", "2020", "2021"],
+            correctAnswer: "2021"
+        },
+        {
+            question: "Who is the Vice President serving alongside Joe Biden as of 2024?",
+            options: ["Kamala Harris", "Mike Pence", "Joe Biden does not have a Vice President", "Nancy Pelosi"],
+            correctAnswer: "Kamala Harris"
+        },
+    ],
+
+    '2024-04-04': [
+        {
+            question: "Which of the following appliances consumes the most electricity in a typical household?",
+            options: ["LED Bulbs", "Refrigerator", "Washing Machine", "Television"],
+            correctAnswer: "Refrigerator"
+        },
+        {
+            question: "What is the purpose of using power strips with multiple outlets?",
+            options: ["To consume more power", "To reduce clutter", "To overload circuits", "To waste electricity"],
+            correctAnswer: "To reduce clutter"
+        },
+        {
+            question: "Which energy-saving practice involves adjusting the thermostat when you're away from home?",
+            options: ["Phantom Power Reduction", "Insulation", "Programmable Thermostats", "Solar Panels"],
+            correctAnswer: "Programmable Thermostats"
+        },
+        {
+            question: "What is the recommended temperature setting for water heaters to save energy?",
+            options: ["50°C", "60°C", "70°C", "80°C"],
+            correctAnswer: "50°C"
+        },
+        {
+            question: "Which type of light bulb is more energy-efficient compared to traditional incandescent bulbs?",
+            options: ["Incandescent Bulbs", "Fluorescent Bulbs", "Candle Bulbs", "Halogen Bulbs"],
+            correctAnswer: "Fluorescent Bulbs"
+        },
+    ],
+
+    '2024-04-05': [
+        {
+            question: "Which international agreement aims to address climate change by limiting global warming to well below 2 degrees Celsius above pre-industrial levels?",
+            options: ["Montreal Protocol", "Paris Agreement", "Kyoto Protocol", "Copenhagen Accord"],
+            correctAnswer: "Paris Agreement"
+        },
+        {
+            question: "What greenhouse gas is primarily responsible for the warming of the Earth's atmosphere?",
+            options: ["Methane", "Carbon Dioxide", "Nitrous Oxide", "Ozone"],
+            correctAnswer: "Carbon Dioxide"
+        },
+        {
+            question: "Which of the following is a consequence of climate change?",
+            options: ["Increased Ice Cover", "Rising Sea Levels", "Global Cooling", "Expansion of Arctic Habitats"],
+            correctAnswer: "Rising Sea Levels"
+        },
+        {
+            question: "What is the term for the process by which the Earth's average temperature increases due to the buildup of greenhouse gases?",
+            options: ["Global Warming", "Ozone Depletion", "Acid Rain", "Deforestation"],
+            correctAnswer: "Global Warming"
+        },
+        {
+            question: "Which renewable energy source is considered a key solution to mitigate climate change?",
+            options: ["Coal", "Natural Gas", "Solar Power", "Nuclear Power"],
+            correctAnswer: "Solar Power"
+        },
+    ],
+
+    '2024-04-06': [
+        {
+            question: "What is the name of the largest dinosaur ever discovered?",
+            options: ["Tyrannosaurus rex", "Brachiosaurus", "Stegosaurus", "Triceratops"],
+            correctAnswer: "Argentinosaurus"
+        },
+        {
+            question: "Which group of dinosaurs is known for their long necks and herbivorous diet?",
+            options: ["Carnosaurs", "Sauropods", "Theropods", "Ornithopods"],
+            correctAnswer: "Sauropods"
+        },
+        {
+            question: "The Tyrannosaurus rex is often referred to by what nickname?",
+            options: ["Thunder Lizard", "Swift Hunter", "King of the Skies", "Ruthless Predator"],
+            correctAnswer: "T. rex"
+        },
+        {
+            question: "In which geological period did dinosaurs first appear?",
+            options: ["Jurassic", "Cretaceous", "Triassic", "Paleogene"],
+            correctAnswer: "Triassic"
+        },
+        {
+            question: "What is the term for the study of fossils and ancient life, including dinosaurs?",
+            options: ["Paleontology", "Archaeology", "Geology", "Anthropology"],
+            correctAnswer: "Paleontology"
+        },
+    ],
+
+    '2024-04-07': [
+        {
+            "question": "Which Asian country is known as the 'Land of the Rising Sun'?",
+            "options": ["Japan", "China", "India", "South Korea"],
+            "correctAnswer": "Japan"
+        },
+        {
+            "question": "What is the largest desert in Asia?",
+            "options": ["Sahara Desert", "Gobi Desert", "Arabian Desert", "Thar Desert"],
+            "correctAnswer": "Gobi Desert"
+        },
+        {
+            "question": "What is the official language of Indonesia?",
+            "options": ["Malay", "Indonesian", "Javanese", "Tagalog"],
+            "correctAnswer": "Indonesian"
+        },
+        {
+            "question": "Which Asian country is famous for its Angkor Wat temple complex?",
+            "options": ["Thailand", "Cambodia", "Vietnam", "Myanmar"],
+            "correctAnswer": "Cambodia"
+        },
+        {
+            "question": "What is the highest peak in the Himalayas?",
+            "options": ["Kangchenjunga", "Mount Everest", "K2", "Lhotse"],
+            "correctAnswer": "Mount Everest"
+        }
+    ],
+
+    '2024-04-08': [
+        {
+            "question": "How often does a leap year occur in the Gregorian calendar?",
+            "options": ["Every 3 years", "Every 4 years", "Every 5 years", "Every 6 years"],
+            "correctAnswer": "Every 4 years"
+        },
+        {
+            "question": "What is the extra day added to a leap year called?",
+            "options": ["Leap Day", "Extra Day", "Bonus Day", "February 29th"],
+            "correctAnswer": "Leap Day"
+        },
+        {
+            "question": "Which month is shortened by one day in a leap year?",
+            "options": ["January", "March", "April", "February"],
+            "correctAnswer": "February"
+        },
+        {
+            question: "[From previous questions] In which year was Google officially incorporated as a company?",
+            options: ["1995", "1998", "2000", "2003"],
+            correctAnswer: "1998"
+        },
+        {
+            "question": "Which Roman emperor introduced the concept of leap years to the Julian calendar?",
+            "options": ["Julius Caesar", "Augustus", "Nero", "Caligula"],
+            "correctAnswer": "Julius Caesar"
+        }
+    ],
+
+    '2024-04-09': [
+        {
+            question: "Who was the first woman to win a Nobel Prize?",
+            options: ["Marie Curie", "Rosalind Franklin", "Mother Teresa", "Ada Lovelace"],
+            correctAnswer: "Marie Curie"
+        },
+        {
+            question: "In which year did the Berlin Wall fall, symbolizing the end of the Cold War?",
+            options: ["1989", "1991", "1990", "1987"],
+            correctAnswer: "1989"
+        },
+        {
+            question: "What is the capital city of Australia?",
+            options: ["Canberra", "Sydney", "Melbourne", "Brisbane"],
+            correctAnswer: "Canberra"
+        },
+        {
+            question: "Who wrote the famous novel 'To Kill a Mockingbird'?",
+            options: ["Harper Lee", "J.K. Rowling", "Stephen King", "Mark Twain"],
+            correctAnswer: "Harper Lee"
+        },
+        {
+            question: "What is the largest planet in our solar system?",
+            options: ["Jupiter", "Saturn", "Earth", "Mars"],
+            correctAnswer: "Jupiter"
+        }
+    ],
+    
+    '2024-04-25': [
+        {
+            question: "Which international agreement aims to address climate change by limiting global warming to well below 2 degrees Celsius above pre-industrial levels?",
+            options: ["Montreal Protocol", "Paris Agreement", "Kyoto Protocol", "Copenhagen Accord"],
+            correctAnswer: "Paris Agreement"
+        },
+        {
+            question: "What greenhouse gas is primarily responsible for the warming of the Earth's atmosphere?",
+            options: ["Methane", "Carbon Dioxide", "Nitrous Oxide", "Ozone"],
+            correctAnswer: "Carbon Dioxide"
+        },
+        {
+            question: "Which of the following is a consequence of climate change?",
+            options: ["Increased Ice Cover", "Rising Sea Levels", "Global Cooling", "Expansion of Arctic Habitats"],
+            correctAnswer: "Rising Sea Levels"
+        },
+        {
+            question: "What is the term for the process by which the Earth's average temperature increases due to the buildup of greenhouse gases?",
+            options: ["Global Warming", "Ozone Depletion", "Acid Rain", "Deforestation"],
+            correctAnswer: "Global Warming"
+        },
+        {
+            question: "Which renewable energy source is considered a key solution to mitigate climate change?",
+            options: ["Coal", "Natural Gas", "Solar Power", "Nuclear Power"],
+            correctAnswer: "Solar Power"
+        },
+    ],
+
+    '2024-04-26': [
+        {
+            question: "What is the result of the expression 3x + 7 when x = 4?",
+            options: ["15", "19", "21", "28"],
+            correctAnswer: "19"
+        },
+        {
+            question: "What is the value of 12 square?",
+            options: ["144", "133", "256", "185"],
+            correctAnswer: "144"
+        },
+        {
+            question: "If a = 5 and b = 3, what is the value of 2a - 3b?",
+            options: ["1", "3", "7", "9"],
+            correctAnswer: "9"
+        },
+        {
+            question: "What is the solution to the equation 2x + 5 = 11?",
+            options: ["x = 2", "x = 3", "x = 4", "x = 6"],
+            correctAnswer: "x = 3"
+        },
+        {
+            question: "If a triangle has side lengths of 5, 12, and 13, what type of triangle is it?",
+            options: ["Equilateral", "Isosceles", "Scalene", "Right-angled"],
+            correctAnswer: "Right-angled"
+        }
+    ]
+};
